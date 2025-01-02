2030317e92e8c3767525b2732b723a3c81731d6d
Sergei Stadnik <sergeisqa@gmail.com>
2025-01-01 19:11:37 -0700

    Add App.js and other components

---
+++ airdrop-tracker/src/assets/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold">Welcome to Airdrop Tracker</h1>
+    </div>
+  );
+}
+
+export default App;
